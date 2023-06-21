8f5dcc6d1c9cea507366868beac9ab69d4736a79
hirotokaneko <61130777+hirotokaneko@users.noreply.github.com>
2023-06-22 00:09:57 +0900

    Delete analysis directory

--- analysis/500_SatelliteDataEmbedding.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ../data
-
-#mkdir ../sync/full.open.thinned200.frq20/umap
-#python ../scripts/502_DimensionalityReduction.py full open 200 20 adjusted
-#python ../scripts/502_DimensionalityReduction.py full open 200 20 keep
-
